9ea4bb9a2d2a45da741cf44c3bacf72f5b6cc46e
Viprajay Dhali <73271252+viprajay@users.noreply.github.com>
2024-09-24 21:16:01 +0530

    add sorting file

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 9, 1, 2, 3, 4, 8, 7, 6 };
+        sort(arr);
+        print(arr);
+    }
+}
